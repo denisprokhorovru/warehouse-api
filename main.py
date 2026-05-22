bffac8c35ee7b3b1f6706e725ed3d647cd1d41e4
Prohorov Denis <denis.prokhorov.ru@gmail.com>
2026-05-23 02:27:22 +0500

    refactor: удалил старые функции с рецептами

--- main.py
+++ main.py
@@ -28,31 +28,6 @@ def remove_item(warehouse, item_name, quantity):
         return False
 
 
-def cook_omelette(warehouse):
-    if get_quantity(warehouse, "яйца") >= 2 and get_quantity(warehouse, "молоко") >= 1:
-        remove_item(warehouse, "яйца", 2)
-        remove_item(warehouse, "молоко", 1)
-        return {"блюдо": "Омлет", "статус": "Готов"}
-    elif get_quantity(warehouse, "яйца") < 2:
-        return {"статус": "Ошибка", "причина": "Не хватает яиц"}
-    elif get_quantity(warehouse, "молоко") < 1:
-        return {"статус": "Ошибка", "причина": "Не хватает молока"}
-
-
-def cook_salad(warehouse):
-    if get_quantity(warehouse, "огурцы") >= 2 and get_quantity(warehouse, "помидоры") >= 1 and get_quantity(warehouse, "сметана") >= 1:
-        remove_item(warehouse, "огурцы", 2)
-        remove_item(warehouse, "помидоры", 1)
-        remove_item(warehouse, "сметана", 1)
-        return {"блюдо": "Салат", "статус": "Готов"}
-    elif get_quantity(warehouse, "огурцы") < 2:
-        return {"статус": "Ошибка", "Причина": "Не хватает огурцов"}
-    elif get_quantity(warehouse, "помидоры") < 1:
-        return {"статус": "Ошибка", "Причина": "Не хватает помидоров"}
-    elif get_quantity(warehouse, "сметана") < 1:
-        return {"статус": "Ошибка", "Причина": "Не хватает сметаны"}
-
-
 def cook(warehouse, dish_name):
     # 1. Проверка существования рецепта
     if dish_name not in RECIPES:
